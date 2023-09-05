bb6fe4f17e37bf4d241ab2a7cff460118082ebe4
ouzyu <murahiro927@gmail.com>
2023-09-05 18:34:06 +0900

    [Update]ディレクティブ:while文,[Finish]chapter_3_3

--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -9,19 +9,18 @@
 
     <body>
         <h1>Blade/Index</h1>
-        <p>&#064;forディレクティブの例</p>
+        <p>&#064;whileディレクティブの例</p>
         <ol>
-            @foreach ( $data as $item)
-                @if ($loop->first)
-                    <p>※データ一覧</p><ul>
-                @endif
+            @php
+                $counter = 0;
+            @endphp
 
-                <li>No, {{ $loop->iteration }}. {{ $item }}</li>
-                
-                @if ($loop->last)
-                    </ul><p>--ここまで</p>
-                @endif
-            @endforeach
+            @while ($counter < count($data))
+                <li>{{ $data[$counter] }}</li>
+                @php
+                    $counter++;
+                @endphp
+            @endwhile
         </ol>
     </body>
 </html> 
