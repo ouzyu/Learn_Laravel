7e08b46eaf1a931c61ca11b4375282c8aa51db20
ouzyu <murahiro927@gmail.com>
2023-09-06 13:07:09 +0900

    [Add]ビューコンポーザーを追加

--- app/Providers/HelloServiceProvider.php
+++ app/Providers/HelloServiceProvider.php
@@ -6,19 +6,15 @@ use Illuminate\Support\ServiceProvider;
 
 class HelloServiceProvider extends ServiceProvider
 {
-    /**
-     * Register services.
-     */
     public function register(): void
     {
-        //
+       
     }
 
-    /**
-     * Bootstrap services.
-     */
     public function boot(): void
     {
-        //
+        view()->composer('hello.index', function ($view) {
+            $view->with('view_message', 'composer message!');
+        });
     }
 }
