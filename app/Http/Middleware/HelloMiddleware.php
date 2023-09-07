63e97ac2978a0aada460e4cbe88456bbf49cf2a6
ouzyu <murahiro927@gmail.com>
2023-09-08 00:58:54 +0900

    [Update]ミドルウェアの実行

--- app/Http/Middleware/HelloMiddleware.php
+++ app/Http/Middleware/HelloMiddleware.php
@@ -15,6 +15,12 @@ class HelloMiddleware
      */
     public function handle(Request $request, Closure $next): Response
     {
+        $data = [
+            ['name'=>'taro', 'mail'=>'taro@yamada'],
+            ['name'=>'hanako', 'mail'=>'hanako@flower'],
+            ['name'=>'sachiko', 'mail'=>'sachiko@happy'],
+        ];
+        $request->merge(['data'=>$data]);
         return $next($request);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HelloController;
+use App\Http\Middleware\HelloMiddleware;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/hello', [HelloController::class, 'index']);
+Route::get('hello', [HelloController::class, 'index'])->middleware(HelloMiddleware::class);
 Route::post('hello', [HelloController::class, 'post']);
