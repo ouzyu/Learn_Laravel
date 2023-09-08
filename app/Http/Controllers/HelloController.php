8c186aad4cce1d19631204771434b5a4955d4ad7
ouzyu <murahiro927@gmail.com>
2023-09-08 20:18:36 +0900

    [Add]HelloRequestクラスの作成

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Hello;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use App\Http\Requests\HelloRequest;
 
 class HelloController extends Controller
 {
@@ -13,14 +14,8 @@ class HelloController extends Controller
         return view('hello.index', ['msg'=>'フォームを入力:']);
     }
 
-    public function post(Request $request)
+    public function post(HelloRequest $request)
     {
-        $validate_rule = [
-            'name'  => 'required',
-            'mail'  => 'email',
-            'age'   => 'numeric|between:0,150',
-        ];
-        $this->validate($request, $validate_rule);
         return view('hello.index', ['msg'=>'正しく入力されました！']);
     }
 }
